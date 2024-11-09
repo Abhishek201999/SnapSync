920b3c79c6eb87dd8e3dd193f722335b28f45c85
Abhishek Singh <Lucifier-777>
2024-11-09 13:56:48 +0530

    Initial Commit

---
+++ SnapSync/SnapSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapSyncApp.swift
+//  SnapSync
+//
+//  Created by Abhishek Singh1 on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SnapSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
