ab773dd494bcf694407fe4ad03c850d81c9beb64
Abhishek Singh <Lucifier-777>
2024-11-10 11:37:10 +0530

    some code cleanup

--- SnapSync/SavedImagePage/SavedImagesScreen.swift
+++ SnapSync/SavedImagePage/SavedImagesScreen.swift
@@ -9,55 +9,62 @@ struct SavedImagesScreen: View {
     }
     
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(viewModel.savedImages) { image in
-                    if let img = image.toUIImage() {
-                        VStack {
-                            Image(uiImage: img)
-                                .resizable()
-                                .frame(height: 400)
-                                .frame(maxWidth: .infinity)
-                                .scaledToFit()
-                                .cornerRadius(12)
-                                .padding(.horizontal, 16)
-                                .padding(.vertical, 10)
-                                .overlay(alignment: .bottom) {
-                                    VStack {
-                                        if let progress = viewModel.uploadProgress[image.id] {
-                                            ProgressView(value: progress, total: 1.0)
-                                                .progressViewStyle(LinearProgressViewStyle())
-                                                .padding(.horizontal, 16)
-                                                .padding(.vertical, 10)
-                                        }
-                                        Text(image.uploadStatus)
-                                            .padding(.horizontal, 8)
-                                            .padding(.vertical, 10)
-                                            .background(Color.black.opacity(0.7))
-                                            .foregroundColor(.white)
-                                            .cornerRadius(8)
-                                        
-                                        if image.uploadStatus == "failed" {
-                                            Button("Retry Upload") {
-                                                viewModel.retryUpload(for: image)
+        if viewModel.savedImages.isEmpty {
+            VStack {
+                Text("No images are stored locally")
+                    .font(.title)
+            }
+        } else {
+            ScrollView {
+                LazyVStack {
+                    ForEach(viewModel.savedImages) { image in
+                        if let img = image.toUIImage() {
+                            VStack {
+                                Image(uiImage: img)
+                                    .resizable()
+                                    .frame(height: 400)
+                                    .frame(maxWidth: .infinity)
+                                    .scaledToFit()
+                                    .cornerRadius(12)
+                                    .padding(.horizontal, 16)
+                                    .padding(.vertical, 10)
+                                    .overlay(alignment: .bottom) {
+                                        VStack {
+                                            if let progress = viewModel.uploadProgress[image.id] {
+                                                ProgressView(value: progress, total: 1.0)
+                                                    .progressViewStyle(LinearProgressViewStyle())
+                                                    .padding(.horizontal, 16)
+                                                    .padding(.vertical, 10)
                                             }
-                                            .padding()
-                                            .background(Color.blue)
-                                            .foregroundColor(.white)
-                                            .cornerRadius(8)
-                                            .padding(.top, 8)
+                                            Text(image.uploadStatus)
+                                                .padding(.horizontal, 8)
+                                                .padding(.vertical, 10)
+                                                .background(Color.black.opacity(0.7))
+                                                .foregroundColor(.white)
+                                                .cornerRadius(8)
+                                            
+                                            if image.uploadStatus == "failed" {
+                                                Button("Retry Upload") {
+                                                    viewModel.retryUpload(for: image)
+                                                }
+                                                .padding()
+                                                .background(Color.blue)
+                                                .foregroundColor(.white)
+                                                .cornerRadius(8)
+                                                .padding(.top, 8)
+                                            }
+                                            
                                         }
-                                        
+                                        .padding(.vertical,16)
                                     }
-                                    .padding(.vertical,16)
-                                }
+                            }
                         }
                     }
                 }
             }
-        }
-        .onAppear {
-            viewModel.uploadPendingImages()
+            .onAppear {
+                viewModel.uploadPendingImages()
+            }
         }
     }
 }
